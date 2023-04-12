84928f1f026e9af336d385b10dd11aad372aeda3
markgmz92 <markgmz92@gmail.com>
2023-04-12 18:02:17 +0800

    Added search functionality

--- app/page.js
+++ app/page.js
@@ -1,11 +1,33 @@
+'use client';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import LoadingPage from './loading';
 import Courses from './components/Courses';
+import CourseSearch from './components/CourseSearch';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
   return (
     <div>
       <h1>Welcome To Mark's Repo</h1>
-      <Courses />
+      <CourseSearch getSearchResults={(results) => setCourses(results)} />
+      <Courses courses={courses} />
     </div>
   );
 };
