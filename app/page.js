b96b8c1cf3e4a843a7269434136d3d16086107a9
markgmz92 <markgmz92@gmail.com>
2023-04-12 17:31:15 +0800

    Display courses on the home page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,11 @@
 import Link from 'next/link';
+import Courses from './components/Courses';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome To Mark's Repo</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
-        dolorem quae culpa tempora facilis commodi asperiores, saepe cupiditate
-        eaque qui modi consequatur dolores fuga inventore doloremque mollitia
-        obcaecati repellat at! Reiciendis incidunt ea et vero animi in dolorum
-        fugit cumque laborum perspiciatis? Molestiae dolorem quaerat deleniti
-        quisquam impedit beatae delectus.
-      </p>
+      <Courses />
     </div>
   );
 };
